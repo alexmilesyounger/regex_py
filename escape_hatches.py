47238301372d5fb82169975a3ec649e51fcc293b
Alex Miles Younger <alex@alexmilesyounger.com>
2015-09-06 22:21:26 -0400

    List of Regex escape sequences

---
+++ escape_hatches.py
@@ -0,0 +1,8 @@
+# \w - matches any Unicode word character
+# \W - matches anything that isn't a Unicode word character
+# \s - matches any whitespace
+# \S - matches anything that isn't whitespace
+# \d - matches any number from 0 to 9
+# \D - matches anything that isn't a number
+# \b - matches word boundaries or edges of a word
+# \B - matches anything that isn't the edge of a word
